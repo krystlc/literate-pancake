ddad64ead1700f6493b475496d0e4c96f72d3336
krystlc <alex.valle@gmail.com>
2023-07-26 02:38:09 -0400

    adding fonts and base styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,27 @@ import forms from "@tailwindcss/forms";
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      /**
+       * rough estimate of colors referencing
+       * a figma slide and https://coolors.co/
+       */
+      "space-cadet": "#282639",
+      "english-violet": "#3E3C4E",
+      "rich-black": "#060417",
+      white: "#ffffff",
+      "light-green": "#6DDD8B",
+      "french-gray": "#B0AFB6",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      /**
+       * TODO: Rubik font is also referenced in the figma,
+       * confirm it's supposed to be implemented or if it's an oversight
+       */
+    },
     extend: {},
   },
   plugins: [forms],
